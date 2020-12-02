37865f70f3c9d785bca1e16de97d191bfb16086d
Jungho781 <72117787+Jungho781@users.noreply.github.com>
2020-12-01 21:42:06 -0800

    Update MovieQueries.java

--- Java3Module5/src/sample/MovieQueries.java
+++ Java3Module5/src/sample/MovieQueries.java
@@ -18,9 +18,6 @@ public class MovieQueries {
             System.out.println("Connecting to database URL: " + URL);
             connection = DriverManager.getConnection(URL);
 
-//            resetExampleDatabase();
-
-//            System.out.println("Creating Movie prepareStatement");
             selectAllMovies = connection.prepareStatement(
                     "Select * From Movies BY ID, Name");
 
@@ -124,20 +121,6 @@ public class MovieQueries {
             return 0;
         }
     }
-
-//    void resetExampleDatabase() {
-//        Statement stmt = null;
-//        try {
-//            stmt = connection.createStatement();
-//            System.out.println("Creating Table - This will throw an exception if the table is already created.");
-//            stmt.execute("CREATE TABLE example (" + "id INTEGER PRIMARY KEY," + "name VARCHAR(255))");
-//            System.out.println("adding values into example table");
-//            stmt.executeUpdate("INSERT INTO example VALUES (1, 'Example Data')");
-//        } catch (SQLException sqlException) {
-//            sqlException.printStackTrace();
-//        }
-//
-//    }
 
     public void close() {
         try {
